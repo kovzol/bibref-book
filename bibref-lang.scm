07281d2df081ad73dc922eeccc725a1ef23f0357
Zoltan Kovacs <kovzol@gmail.com>
2026-08-02 23:31:28 +0200

    Fix hyphenation problem with syntax highlighting

--- bibref-lang.scm
+++ bibref-lang.scm
@@ -6,10 +6,15 @@
   `(,(string->symbol key)
     (keyword "diagram" "extend" "find" "getrefs" "help" "jaccard" "latintext"
        "length" "lookup" "maxresults" "minunique" "nearest" "raw"
-       "search" "statement" "text" "tokens")
-    (declare_module
+       "search" "statement" "Statement" "text" "tokens")
+    (keyword_control "connects" "with" "based" "on" "introduction" "form" "that"
+       "moreover" "fragment" "matches" "differing" "by" "and" "unique" "verbatim"
+       "providing" "an" "overall" "cover" "of")
+    (constant_module
       "LXX" "StatResGNT" "SBLGNT")
-    (constant_module "Genesis" "Exodus" "Leviticus" "Numbers" "Deuteronomy"
+    (variable_module
+      "HunRUF" "Aleppo" "KJV")
+    (constant_identifier "Genesis" "Exodus" "Leviticus" "Numbers" "Deuteronomy"
       "Joshua" "Judges" "Ruth" "I_Samuel" "II_Samuel" "I_Kings" "II_Kings"
       "I_Chronicles" "II_Chronicles" "I_Esdras" "Esra" "Nehemiah" "Esther"
       "Judith" "Tobit" "I_Maccabees" "II_Maccabees" "III_Maccabees" "IV_Maccabees"
@@ -18,7 +23,7 @@
       "Nahum" "Habakkuk" "Zephaniah" "Haggai" "Zechariah" "Malachi" "Isaiah"
       "Jeremiah" "Baruch" "Lamentations" "Epistle_of_Jeremiah" "Ezekiel"
       "Susanna" "Daniel" "Bel_and_the_Dragon" "Odes")
-    (variable_module "Matthew" "Mark" "Luke" "John" "Romans" "Acts"
+    (variable_identifier "Matthew" "Mark" "Luke" "John" "Romans" "Acts"
       "I_Corinthians" "II_Corinthians" "Galatians" "Ephesians" "Philippians" "Colossians"
       "I_Thessalonians" "II_Thessalonians" "I_Timothy" "II_Timothy" "Titus"
       "Philemon" "Hebrews" "Jude" "I_Peter" "II_Peter"
@@ -31,10 +36,12 @@
   (syntax-read-preferences "bibref"))
 
 (define-preferences
-  ("syntax:bibref:none" "red" notify-bibref-syntax)
-  ("syntax:bibref:error" "dark red" notify-bibref-syntax)
-  ("syntax:bibref:keyword" "#000a74" notify-bibref-syntax)
-  ("syntax:bibref:declare_module" "#00010d" notify-bibref-syntax)
-  ("syntax:bibref:constant_module" "#3f2800" notify-bibref-syntax)
-  ("syntax:bibref:variable_module" "#000542" notify-bibref-syntax)
+  ("syntax:bibref:none" "dark red" notify-bibref-syntax)
+  ("syntax:bibref:error" "red" notify-bibref-syntax)
+  ("syntax:bibref:keyword" "#510e01" notify-bibref-syntax)
+  ("syntax:bibref:keyword_control" "#5f1000" notify-bibref-syntax)
+  ("syntax:bibref:constant_module" "#000000" notify-bibref-syntax)
+  ("syntax:bibref:variable_module" "#202020" notify-bibref-syntax)
+  ("syntax:bibref:constant_identifier" "#3f2800" notify-bibref-syntax)
+  ("syntax:bibref:variable_identifier" "#000542" notify-bibref-syntax)
 )
